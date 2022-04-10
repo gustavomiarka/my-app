3f959b660e2ff560457173cfa06a20f6ea71f34c
gustavomiarka <gustavo_miarka@hotmail.com>
2022-04-10 14:35:06 -0300

    desarrollo app

---
+++ src/app/componentes/imag/imag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imag',
+  templateUrl: './imag.component.html',
+  styleUrls: ['./imag.component.css']
+})
+export class ImagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/servicios/porfolio.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PorfolioService {
+
+  constructor() { }
+
+  obtenerDatos(){
+    console.log("el servicio corre");
+  }
+}
